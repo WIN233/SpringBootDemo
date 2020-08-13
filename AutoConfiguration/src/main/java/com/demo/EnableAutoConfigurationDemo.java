2ddfac49d8eb57f9e08e23e71315affcaa174ec8
WIN233 <1374862606@qq.com>
2020-08-13 16:43:11 +0800

    springboot自动装配相关示例

--- AutoConfiguration/src/main/java/com/demo/EnableAutoConfigurationDemo.java
+++ AutoConfiguration/src/main/java/com/demo/EnableAutoConfigurationDemo.java
@@ -2,14 +2,18 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableAutoConfiguration
 public class EnableAutoConfigurationDemo {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(EnableAutoConfigurationDemo.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(EnableAutoConfigurationDemo.class, args);
 
+
+        Object hello = context.getBean("hello");
+        System.out.println(hello);
 
 
     }
